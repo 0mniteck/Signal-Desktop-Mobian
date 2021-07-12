c49ed2654d4f441514a37158f01596305e4c412a
Shant <shantpat@gmail.com>
2021-07-11 19:21:17 -0700

    Build Everything Together

--- buildscript.sh
+++ buildscript.sh
@@ -5,11 +5,11 @@ apt install snapd
 echo "Starting Build on "$(date)
 echo
 
-# echo "Entering ringrtc/"
-# pushd ringrtc
-# ./re-run.sh
-# popd
-# ls -la libringrtc-arm64.node
+echo "Entering ringrtc/"
+pushd ringrtc
+./re-run.sh
+popd
+ls -la libringrtc-arm64.node
 
 ./re-run.sh
 ls -la builds/release/
