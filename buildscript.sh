4dc550411f788ee9b4edf01198cfd9883a2fac2b
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-11-11 12:30:39 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -11,8 +11,6 @@ echo
 sudo screen bash -c './re-run.sh'
 ls -la builds/release/
 
-sudo snap remove --purge docker
-
 git status && git add -A && git status
 read -p "Continue -->"
 git commit -a -S -m "Successful Build of Signed Release 6.37.0" && git push
