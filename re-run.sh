81377bfc6a7a6970ca4b7ddb69192c44837b5810
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-11-11 12:52:28 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-snap remove docker --purge && snap install docker && ufw disable && sleep 10 && docker run --cpus=$(nproc) --rm --privileged multiarch/qemu-user-static --reset -p yes && docker build -t pinephone . && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && ufw enable && rm -fr builds/release/linux-* && snap remove docker --purge
+snap remove docker --purge && snap install docker && ufw disable && sleep 10 && docker build -t pinephone . && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && rm -fr builds/release/linux-* && snap remove docker --purge
+ufw enable
 read -p "Continue -->"
