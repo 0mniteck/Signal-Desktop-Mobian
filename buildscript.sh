1e4edbec113c835d70cdd79057860ca4dd2a4519
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-11-11 12:41:00 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -12,5 +12,4 @@ sudo screen bash -c './re-run.sh'
 ls -la builds/release/
 
 git status && git add -A && git status
-read -p "Continue -->"
 git commit -a -S -m "Successful Build of Signed Release 6.37.0" && git push
