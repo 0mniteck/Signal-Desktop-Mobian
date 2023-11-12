1662f2c73196390cf88201dd7b6835102573b2cd
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-11-11 18:22:26 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-snap remove docker --purge && snap install docker && ufw disable && sleep 10 && cp /etc/keys/.private.key .private.key && docker build -t pinephone . && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && rm -f .private.key && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && rm -fr builds/release/linux-* && snap remove docker --purge
+snap remove docker --purge && snap install docker && ufw disable && sleep 10 && cp /etc/keys/.private.key .private.key && docker build -t pinephone . && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && shred .private.key && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && rm -fr builds/release/linux-* && snap remove docker --purge
 ufw enable
 read -p "Continue -->"
