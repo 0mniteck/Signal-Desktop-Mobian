266ec96b4587032e38f62b8bb8fa7d7b8eacdf17
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2023-11-12 16:06:56 -0800

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -2,14 +2,14 @@
 
 rm -f -r /var/snap/docker/*
 umount -f /dev/mapper/Luks-Signal
-cryptsetup close /dev/mmcblk1
+cryptsetup close /dev/mapper/Luks-Signal
 rm -f -r /var/snap/docker
 snap remove docker --purge
 cryptsetup open /dev/mmcblk1 Luks-Signal && mkdir /var/snap/docker && mount /dev/mapper/Luks-Signal /var/snap/docker && rm -f -r /var/snap/docker/*
 snap install docker && ufw disable && sleep 10 && cp /etc/keys/.private.key .private.key && docker build -t pinephone . && shred .private.key && rm -f .private.key && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && rm -fr builds/release/linux-*
 rm -f -r /var/snap/docker/*
 umount -f /dev/mapper/Luks-Signal
-cryptsetup close /dev/mmcblk1
+cryptsetup close /dev/mapper/Luks-Signal
 rm -f -r /var/snap/docker
 sleep 10
 snap remove docker --purge
