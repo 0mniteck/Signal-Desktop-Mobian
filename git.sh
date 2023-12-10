4312a9529d9a85b2742687b31f94ccddefacd035
0mniteck <10482171+0mniteck@users.noreply.github.com>
2023-12-10 22:15:41 +0000

    Successful Build of Signed Release 6.41.0

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git status && git add -A && git status
-git commit -a -S -m "Successful Build of Signed Release 6.40.0" && git push --set-upstream origin master
+git commit -a -S -m "Successful Build of Signed Release 6.41.0" && git push --set-upstream origin master
