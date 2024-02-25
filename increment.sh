663e774b4aac6ad65268262c60fc145da7963ed7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-02-24 18:11:11 -0800

    Successful Build of Signed Release 6.48.1

--- increment.sh
+++ increment.sh
@@ -5,7 +5,7 @@ Version_Number=$(echo "$Last_Version_Number + .02" | bc -l)
 Last_Branch_Number=$Last_Version_Number.x
 Branch_Number=$Version_Number.x
 
-sed -i s/signal-desktop_$(echo $Last_Version_Number).0_arm64.deb/signal-desktop_$(echo $Version_Number).1_arm64.deb/g docs/_layouts/default.html
+sed -i s/signal-desktop_$(echo $Last_Version_Number).1_arm64.deb/signal-desktop_$(echo $Version_Number).0_arm64.deb/g docs/_layouts/default.html
 
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/README.md
 
