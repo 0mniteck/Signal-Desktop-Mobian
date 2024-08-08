8ece1166173d3a84a18acddbebdab34e48eef42e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-07 17:49:52 -0700

    Update increment.sh

--- increment.sh
+++ increment.sh
@@ -3,7 +3,7 @@
 Last_Version_Number=7.18
 Version_Number=$(echo "$Last_Version_Number + .01" | bc -l)
 
-sed -i s/signal-desktop_$(echo $Last_Version_Number).1_arm64.deb/signal-desktop_$(echo $Version_Number).0_arm64.deb/g docs/_layouts/default.html
+sed -i s/signal-desktop_$(echo $Last_Version_Number).0_arm64.deb/signal-desktop_$(echo $Version_Number).0_arm64.deb/g docs/_layouts/default.html
 
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/README.md
 
