575296ba2a95d5b332b3aaba15adc19514c5ca5a
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-07 19:10:08 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -5,16 +5,17 @@ umount -f /dev/mapper/Luks-Signal
 cryptsetup close /dev/mapper/Luks-Signal
 rm -f -r /var/snap/docker
 snap remove docker --purge
-cryptsetup open /dev/mmcblk1 Luks-Signal && mkdir /var/snap/docker && mount /dev/mapper/Luks-Signal /var/snap/docker && rm -f -r /var/snap/docker/* && chown root:root /var/snap/docker
+systemd-cryptsetup attach Luks-Signal /dev/mmcblk1 && mkdir /var/snap/docker && mount /dev/mapper/Luks-Signal /var/snap/docker && rm -f -r /var/snap/docker/* && chown root:root /var/snap/docker
+read -p "Secure Storage Mounted: Continue -->"
 snap install docker && ufw disable && sleep 10 && cp /etc/keys/.private.key .private.key && docker build -t pinephone . && shred .private.key && rm -f .private.key && docker run --cpus=$(nproc) --name pinephone -it pinephone bash -c /signal-buildscript.sh && rm -fr builds/release/ && mkdir -p builds/release && docker cp pinephone:/Signal-Desktop/release/ builds/ && rm -fr builds/release/linux-*
 snap disable docker
 rm -f -r /var/snap/docker/*
 sleep 10
 umount -f /dev/mapper/Luks-Signal
-cryptsetup close /dev/mapper/Luks-Signal
+systemd-cryptsetup detach /dev/mapper/Luks-Signal
 rm -f -r /var/snap/docker
 sleep 10
 snap remove docker --purge
 snap remove docker --purge
 ufw -f enable
-read -p "Continue -->"
+read -p "Close Screen Session: Continue to Signing-->"
