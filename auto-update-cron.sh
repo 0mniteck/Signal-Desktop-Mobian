89d4420d7c74b0b44af9603b04c007b8b8b145c8
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-28 13:25:47 -0700

    Update auto-update-cron.sh

--- auto-update-cron.sh
+++ auto-update-cron.sh
@@ -5,11 +5,11 @@
 # Runs cron job if exists otherwise creates cron job
 
 
-# Cron job
+# Cron job  - WORK IN PROGRESS
 # Get current version installed if installed otherwise stop
 
 # Check this git repo for most recent version if newer release download to /tmp otherwise stop
-
+wget https://raw.githubusercontent.com/0mniteck/Signal-Desktop-Mobian/master/builds/release/latest-linux-arm64.yml
 # Stop any running instances of signal-desktop
 
 # Install and restart if instance was runnning
