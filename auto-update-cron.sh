93e1b671ab35c421ae5fbbf61a7c78cddccff0b8
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-08-28 13:18:43 -0700

    Create auto-update-cron.sh

---
+++ auto-update-cron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script to add a cron job to check for and download the most recent version from this git repo
+
+# Runs cron job if exists otherwise creates cron job
+
+
+# Cron job
+# Get current version installed if installed otherwise stop
+
+# Check this git repo for most recent version if newer release download to /tmp otherwise stop
+
+# Stop any running instances of signal-desktop
+
+# Install and restart if instance was runnning
