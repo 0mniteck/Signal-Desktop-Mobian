e2ceea5ceb000b30e8c3b41666ec9f9c05a77d96
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-09-23 15:10:28 -0700

    Update signal-buildscript.sh

--- signal-buildscript.sh
+++ signal-buildscript.sh
@@ -6,6 +6,7 @@ echo "Entering /Signal-Desktop"
 pushd /Signal-Desktop
 sed -i 's/"electron": "32.1.0"/"electron": "32.1.2"/' package.json
 sed -i 's/"electron": "32.1.0"/"electron": "32.1.2"/' package-lock.json
+sed -i 's/"canvas": "$nop"/"canvas": "next"/' package-lock.json
 sed -i 's/"canvas": "^2.6.1"/"canvas": "next"/' package-lock.json
 git-lfs install
 nvm use
