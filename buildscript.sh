a0f4a131b8fc99d5a742445e3424083ed0691d6e
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-10-30 17:56:28 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -8,7 +8,7 @@ git-lfs install
 echo "Starting Build "$(date -u '+on %D at %R UTC')
 echo
 
-./increment.sh
+./increment.sh $1
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh public'
 cp /tmp/builder.log builds/release/builder.log && rm -f /tmp/builder.log
 ls -la builds/release/
