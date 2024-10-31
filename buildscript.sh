e17f6ae7dbe76fe1d354bb1ba553060b4863edc2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-10-31 16:48:46 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -9,7 +9,7 @@ echo "Starting Build "$(date -u '+on %D at %R UTC')
 echo
 
 ./increment.sh $1
-sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh public $2 $3'
+sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh public \$2 \$3'
 cp /tmp/builder.log builds/release/builder.log && rm -f /tmp/builder.log
 ls -la builds/release/
 
