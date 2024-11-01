d52f41e85ffdb6def64f325b95aa155fb62aff06
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-10-31 21:26:37 -0700

    Update increment.sh

--- increment.sh
+++ increment.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-
 if [ "$1" != "" ]; then
 Last_Version_Number=7.31
 Version_Number=$(echo "$Last_Version_Number + $1" | bc -l)
-
-sed -i s/signal-desktop_$(echo $Last_Version_Number).0_arm64.deb/signal-desktop_$(echo $Version_Number).0_arm64.deb/g docs/_layouts/default.html
-
+sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/_layouts/default.html
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/README.md
-
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g README.md
-
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g git.sh
-
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g Dockerfile
-
 sed -i s/Last_Version_Number=$Last_Version_Number/Last_Version_Number=$Version_Number/g increment.sh
-
 echo Version Incremented From v$Last_Version_Number To v$Version_Number
-
 fi
 exit
