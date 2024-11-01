aefb39f3bfda51deede5189f1cd9b853969570d7
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-10-31 19:40:08 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -12,6 +12,7 @@ echo
 sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh public '$(($2))' '$3
 cp /tmp/builder.log builds/release/builder.log && rm -f /tmp/builder.log
 echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release/release.sha512sum
+awk '{a[i++]=$0}END{for(j=0;j<i-2;j++)print a[j];print a[i-1];print a[i-2]}' builds/release/release.sha512sum > tmp && mv tmp builds/release/release.sha512sum
 ls -la builds/release/
 
 ./git.sh
