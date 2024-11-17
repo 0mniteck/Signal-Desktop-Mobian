d28cda39865a2443b6fa429826c4a04b5a0d2eca
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-16 20:13:16 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 git status && git add -A && git status
-if [ "$1" != "" ]; then
-  git commit -a -S -m "Successful Build of Reproducible Release 7.31.0" && git push --set-upstream origin $1
-fi
+git commit -a -S -m "Successful Build of Reproducible Release 7.31.0" && git push --set-upstream origin $1
 if [ "$2" != "" ]; then
   git tag -a "$2" -s -m "Tagged Release $2" && sleep 5 && git push origin "$2"
 fi
