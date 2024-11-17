4d6949d5da31201320347194e04e4f41e622473d
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-16 17:18:18 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git status && git add -A && git status
-git commit -a -S -m "Successful Build of Reproducible Release 7.31.0" && git push --set-upstream origin master
+git commit -a -S -m "Successful Build of Reproducible Release 7.31.0" && git push --set-upstream origin debug
