389effac81cdfcf12249193b3a2a34fa3e1e0bf3
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-16 20:12:25 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -9,16 +9,28 @@ while getopts ":i:d:m:p:r:t:" opt; do
             EPOCH="$OPTARG"
             ;;
         m)
-            MOUNT="$OPTARG"
+            if [ "$OPTARG" = "" ]; then
+                MOUNT="no"
+            else
+                MOUNT="$OPTARG"
+            fi
             ;;
         p)
-            BRANCH="$OPTARG"
+            if [ "$OPTARG" = "" ]; then
+                BRANCH="debug"
+            else
+                BRANCH="$OPTARG"
+            fi
             ;;
         r)
             TAG="$OPTARG"
             ;;
         t)
-            TEST="$OPTARG"
+            if [ "$OPTARG" = "" ]; then
+                TEST="no"
+            else
+                TEST="$OPTARG"
+            fi
             ;;
         \?)
             echo "Invalid option: -$opt" >&2
@@ -39,7 +51,7 @@ echo "Run Tests: $TEST"
 sudo apt install bc git-lfs screen snapd systemd-cryptsetup -y
 git remote remove origin && git remote add origin git@Signal:0mniteck/Signal-Desktop-Mobian.git && git-lfs install
 ./increment.sh $INC
-sudo screen -L -Logfile builder.log bash -c './re-run.sh public '$(($DATE))' '$MOUNT' '$TEST && mv builder.log builds/release/builder.log
+sudo screen -L -Logfile builder.log bash -c './re-run.sh public '$(($EPOCH))' '$MOUNT' '$TEST && mv builder.log builds/release/builder.log
 echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release/release.sha512sum
 awk '{a[i++]=$0}END{for(j=0;j<i-2;j++)print a[j];print a[i-1];print a[i-2]}' builds/release/release.sha512sum > tmp && mv tmp builds/release/release.sha512sum
 ls -la builds/release/
