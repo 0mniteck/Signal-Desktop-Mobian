0c08858656075f1fd888e2cf3fc1bb7f7dc979a4
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-16 19:49:19 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -33,17 +33,11 @@ echo "Push to Branch: $BRANCH"
 echo "Tag Release: $TAG"
 
 sudo apt install bc git-lfs screen snapd systemd-cryptsetup -y
-snap install grype --classic
-
-git remote remove origin && git remote add origin git@Signal:0mniteck/Signal-Desktop-Mobian.git
-git-lfs install
-
+git remote remove origin && git remote add origin git@Signal:0mniteck/Signal-Desktop-Mobian.git && git-lfs install
 ./increment.sh $INC
-sudo screen -L -Logfile /tmp/builder.log bash -c './re-run.sh public '$(($DATE))' '$MOUNT
-cp /tmp/builder.log builds/release/builder.log && rm -f /tmp/builder.log
+sudo screen -L -Logfile builder.log bash -c './re-run.sh public '$(($DATE))' '$MOUNT && mv builder.log builds/release/builder.log
 echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release/release.sha512sum
 awk '{a[i++]=$0}END{for(j=0;j<i-2;j++)print a[j];print a[i-1];print a[i-2]}' builds/release/release.sha512sum > tmp && mv tmp builds/release/release.sha512sum
 ls -la builds/release/
-
 ./git.sh $BRANCH $TAG
 cd ..
