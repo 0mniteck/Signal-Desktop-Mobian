8d077124514bc1bfa32b5db91572e59ca8c3a326
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2024-11-20 18:04:00 -0800

    Update increment.sh

--- increment.sh
+++ increment.sh
@@ -4,7 +4,7 @@ Last_Version_Number=7.33
 Version_Number=$(echo "$Last_Version_Number + $1" | bc -l)
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/_layouts/default.html
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g docs/README.md
-sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g README.md
+sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g readme.md
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g git.sh
 sed -i s/$(echo $Last_Version_Number)/$(echo $Version_Number)/g Dockerfile
 sed -i s/Last_Version_Number=$Last_Version_Number/Last_Version_Number=$Version_Number/g increment.sh
