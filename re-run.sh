3657965634c978c6c6975220afa9e602636d8bf2
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-04-04 22:07:46 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -44,7 +44,7 @@ docker buildx create --name signal-builder --driver-opt "network=host" --bootstr
 docker buildx build --tag signal-desktop --load \
   --build-arg SOURCE_DATE_EPOCH=$source_date_epoch \
   --build-arg SOURCE=0mniteck/debian-slim:03-17-2025@sha256:81999bb4392c46ce50b4649dc45cdfe75435e15a89d381aefa871eb285ddf454 \
-  --build-arg NODE_VERSION=20.18.2 \
+  --build-arg NODE_VERSION=22.14.0 \
   --build-arg NVM_VERSION=0.40.2 \
   --build-arg NPM_VERSION=10.2.5 .
 
