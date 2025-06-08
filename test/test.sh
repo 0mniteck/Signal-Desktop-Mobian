784f2a1a140ccd1b16c075ac88903765168d3038
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-06-07 20:01:34 -0700

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 snap install docker --revision=3065
-docker buildx create --name signal-builder --driver-opt "network=host" --bootstrap --use
+docker buildx create --name signal-builder --bootstrap --use
 docker buildx build --tag signal-desktop --load \
   --build-arg SOURCE=0mniteck/debian-slim:05-20-2025@sha256:da7aa4f50de8c010c1822d0287f24ffbd1147cb37874a1c7e4cd896ece00ebb7 .
 
