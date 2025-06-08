dc669765b47427093065c0b3e6715403c30803ff
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-06-07 18:39:15 -0700

    Create test.sh

---
+++ test/test.sh
@@ -0,0 +1,8 @@
+snap install docker --revision=3065
+docker buildx create --name signal-builder --driver-opt "network=host" --bootstrap --use
+docker buildx build --tag signal-desktop --load \
+  --build-arg SOURCE=0mniteck/debian-slim:05-20-2025@sha256:da7aa4f50de8c010c1822d0287f24ffbd1147cb37874a1c7e4cd896ece00ebb7 .
+
+docker run -it --cpus=$(nproc) \
+  --name signal-desktop \
+  --user "$(id -u):$(id -g)" signal-desktop
