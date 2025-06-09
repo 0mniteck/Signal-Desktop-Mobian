4440a4517b624f142d60d32a480257aef93bc77c
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-06-08 19:42:57 -0700

    Update re-run.sh

--- re-run.sh
+++ re-run.sh
@@ -75,6 +75,7 @@ rm -f -r /var/snap/docker
 sleep 5
 snap remove docker --purge
 snap remove docker --purge
+rm -f -r /var/lib/snapd/cache/*
 networkctl delete docker0
 mkdir -p "$HOME/syft" && TMPDIR="$HOME/syft" syft / --select-catalogers debian --exclude /etc -o spdx-json=builds/release/ubuntu.25.04.spdx.json && rm -f -r "$HOME/syft"
 snap remove syft --purge && rm -f -r $HOME/.cache/syft
