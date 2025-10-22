a85d10796421054e2ecce68b0b2c7645ee2e29d6
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2025-10-22 15:26:54 -0700

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-while getopts ":i:d:m:p:r:t:" opt; do
+while getopts ":c:i:d:m:p:r:t:" opt; do
     case $opt in
-        i)
-            INC="$OPTARG"
+        c) # Cross Compile: yes/No
+            CROSS="$OPTARG"
             ;;
-        d)
+        d) # Date: source_date_epoch
             EPOCH="$OPTARG"
             ;;
-        m)
+        i) # Increment: .version
+            INC="$OPTARG"
+            ;;
+        m) # Mount Luks partition: mmcblk1p1
             MOUNT="$OPTARG"
             ;;
-        p)
+        p) # Push-branch: debug
             BRANCH="$OPTARG"
             ;;
-        r)
+        r) # Release-tag: tagname
             TAG="$OPTARG"
             ;;
-        t)
+        t) # run-Tests: yes/No
             TEST="$OPTARG"
             ;;
         \?)
@@ -28,15 +31,16 @@ while getopts ":i:d:m:p:r:t:" opt; do
             ;;
     esac
 done
-if [ "$MOUNT" = "" ]; then
-    MOUNT="no"
-fi
 if [ "$BRANCH" = "" ]; then
     BRANCH="debug"
 fi
 if [ "$TEST" = "" ]; then
     TEST="no"
 fi
+if [ "$CROSS" = "" ]; then
+    CROSS="no"
+fi
+echo "Cross Compile: $CROSS"
 echo "Increment: $INC"
 echo "Override Source Epoch: $EPOCH"
 echo "Mount /dev/mmcblk1: $MOUNT"
@@ -47,7 +51,7 @@ sleep 5
 sudo apt install ansifilter bc git-lfs screen snapd systemd-cryptsetup -y
 git remote remove origin && git remote add origin git@Signal:0mniteck/Signal-Desktop-Mobian.git && git-lfs install
 ./increment.sh $INC
-sudo screen -L -Logfile builder.log bash -c './re-run.sh public '$(($EPOCH))' '$MOUNT' '$TEST && mv builder.log builds/release/builder.log
+sudo screen -L -Logfile builder.log bash -c './re-run.sh public '$(($EPOCH))' '$MOUNT' '$TEST' '$CROSS && mv builder.log builds/release/builder.log
 echo "# Base Build System: $(uname -o) $(uname -r) $(uname -p) $(lsb_release -ds) $(lsb_release -cs) $(uname -v)"  >> builds/release/release.sha512sum
 awk '{a[i++]=$0}END{for(j=0;j<i-2;j++)print a[j];print a[i-1];print a[i-2]}' builds/release/release.sha512sum > tmp && mv tmp builds/release/release.sha512sum
 ls -la builds/release/
