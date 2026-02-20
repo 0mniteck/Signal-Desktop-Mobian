97396680cbda7a02eaf2e47dff544272c44fb5c0
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-19 23:13:48 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -386,11 +386,6 @@ systemctl --user status docker.dockerd --all --no-pager -n 150 > $rootless_path/
 
 source $rootless_path/env-rootless.exp
 
-docker() {
-  echd=\"\$@\"
-  $docker \"\$echd\"
-}
-
 quiet \"\$docker info | grep rootless > $rootless_path/rootless.status\"
 if [[ \"\$(grep root $rootless_path/rootless.status)\" != *rootless* ]]; then
   echo \"Rootless Docker Failed\" && echo && exit 1
@@ -445,10 +440,10 @@ else
 fi
 
 if [[ \"\$(uname -m)\" == \"aarch64\" ]]; then
-  docker run --privileged --rm tonistiigi/binfmt:qemu-v10.0.4-59 --install amd64
+  $docker run --privileged --rm tonistiigi/binfmt:qemu-v10.0.4-59 --install amd64
   echo
 elif [[ \"\$(uname -m)\" == \"x86_64\" ]]; then
-  docker run --privileged --rm tonistiigi/binfmt:qemu-v10.0.4-59 --install arm64
+  $docker run --privileged --rm tonistiigi/binfmt:qemu-v10.0.4-59 --install arm64
   echo
 else
   echo 'Unknown Architecture '\$(uname -m)
@@ -459,7 +454,6 @@ source modules
 
 mkdir -p Results && pushd Results
   scan_using_grype ubuntu \"/ --select-catalogers debian\"
-  cat ../*/*.digest > image.digests
   cat image.digests >> readme.md && cat readme.md && echo
 popd
 
