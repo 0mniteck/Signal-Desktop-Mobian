bb8061c2fc11037202c029413da8e8e32e237979
Shant Tchatalbachian <10482171+0mniteck@users.noreply.github.com>
2026-02-25 04:00:53 -0800

    Update buildscript.sh

--- buildscript.sh
+++ buildscript.sh
@@ -226,7 +226,7 @@ else
   declare -- PUSH='"--push"'
 fi
 if [ "$CROSS" = "yes" ]; then
-  declare -- CROSS='"--platform linux/arm64,linux/amd64 -t multi-platform"'
+  declare -- CROSS='"--platform linux/arm64,linux/amd64"'
 fi
 
 pushd $docker_data > /dev/null
